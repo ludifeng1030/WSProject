656fe0131f6d36e8c9da217411cd8471cb8c8108
yuranus <77255167+yuranusduke@users.noreply.github.com>
2022-05-02 17:27:31 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-python main.py main \
---subject=['Computer Science','Physics','Mathematics'] \
---ssubject=['Computing Research Repository','Physics','Mathematics'] \
---sssubject=['Computer Vision and Pattern Recognition','Machine Learning','Applied Physics','functional analysis'] \
---limit=100 \
---scrape_each_author=True \
---crawl=True \
---analyse=True \
---page_limit=True
